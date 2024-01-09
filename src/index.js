943ba9604a5b1f599a459628db7747a1f07217c0
Alexander Sosov <luisodiz@yandex.ru>
2024-01-09 13:02:02 +0300

    feat: создать структуру классов

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import './module';
 import './scss/index.scss';
+import {Excel} from '@/components/excel/Excel';
+
+const excel = new Excel('#app', {
+  components: []
+});
+console.log('excel', excel);
